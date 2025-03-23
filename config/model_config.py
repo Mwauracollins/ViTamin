fac8b35a3c246fee4845c49f593e1057bf9720b0
Mwaura Collins <eb1.56094.21@student.chuka.ac.ke>
2025-03-23 11:25:38 +0300

    Add ModelConfig dataclass for model configuration parameters

---
+++ config/model_config.py
@@ -0,0 +1,19 @@
+from dataclasses import dataclass
+
+@dataclass
+class ModelConfig:
+    d_model: int = 768
+    n_layers: int = 12
+    n_heads: int = 12
+    hidden_act: str = 'gelu'
+    layer_norm_eps: int = 1e-12
+    image_size: int = 224
+    patch_size: int = 16
+    n_channels: int = 3
+    qkv_bias = True
+
+    hidden_dim: int = 1536
+
+    att_dropout: int = 0.2 #TODO: CHANGE THIS TO MATCH THE ORIGINAL
+    
+    
